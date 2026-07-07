4e3444b2d885b5e7761761f5684a02f59cc3a7ad
Rhian Resnick <rresnick@fau.edu>
2026-07-07 10:49:40 -0400

    Adding support for comments to specify the model, this will let our ollamas use individual models, improving performance

--- src/sbatch.sh
+++ src/sbatch.sh
@@ -6,8 +6,15 @@
 #SBATCH --time=02:00:00
 #SBATCH --output=~/slurm_litellm/logs/ollama-%j.out
 
+# Used to specify the model to follow...
+MODEL=$(squeue -j $SLURM_JOB_ID -h -o "%C")
+COMMENT=$(squeue -j $SLURM_JOB_ID -h -o "%C")
+
+
+# Generate a unique key to store the model download based on the model provide
+
 # 0. Start a dependency on this node to keep ollama running when this job ends
-sbatch --dependency=afterany:$SLURM_JOB_ID sbatch.sh
+sbatch --dependency=afterany:$SLURM_JOB_ID --comment="$COMMENT" sbatch.sh
 
 # 1. Determine this node's exact network IP or hostname, this is a backup incase squeue don't work in proxy.py
 NODE_IP=$(hostname -I | awk '{print $1}')
@@ -28,8 +35,27 @@ trap cleanup EXIT INT TERM
 # 4. Bind Ollama to all interfaces so the external proxy can talk to it
 export OLLAMA_HOST="0.0.0.0:${PORT}"
 export OLLAMA_KEEP_ALIVE="30m"
-export OLLAMA_MODELS=~/scratch/ollama/models
+
+
+export OLLAMA_MODELS=~/scratch/ollama/models/$(echo $MODEL | sha256)
 mkdir -p $OLLAMA_MODELS
 
 # 5. Execute the server
-~/.local/bin/ollama serve
+# ~/.local/bin/ollama serve
+srun --ntasks=1 --nodes=1 ollama serve &
+
+until curl -s http://${OLLAMA_HOST}/ > /dev/null; do
+    sleep 1
+done
+
+
+echo Ollama is ready
+ollama pull $MODEL
+
+while curl -s http://${OLLAMA_HOST}/ > /dev/null; do
+    sleep 30
+done
+
+echo Ollama is dead Jim
+
+
